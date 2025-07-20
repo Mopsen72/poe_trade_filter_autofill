2704987eff5516a4a67553bc33c9ff0922d6dea8
eugeny <zhiryakoved@72to.ru>
2025-07-20 17:58:53 +0500

    изменение названия конечного файла расширения

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 import path from 'node:path'
-import { crx } from '@crxjs/vite-plugin'
-import { defineConfig } from 'vite'
+import {crx} from '@crxjs/vite-plugin'
+import {defineConfig} from 'vite'
 import zip from 'vite-plugin-zip-pack'
 import react from '@vitejs/plugin-react'
 import manifest from './manifest.config.js'
-import { name, version } from './package.json'
+import {name, version} from './package.json'
 
 export default defineConfig({
-  resolve: {
-    alias: {
-      '@': `${path.resolve(__dirname, 'src')}`,
+    resolve: {
+        alias: {
+            '@': `${path.resolve(__dirname, 'src')}`,
+        },
     },
-  },
-  plugins: [
-      react(),
-    crx({ manifest }),
-    zip({ outDir: 'release', outFileName: `crx-${name}-${version}.zip` }),
-  ],
-  server: {
-    cors: {
-      origin: [
-        /chrome-extension:\/\//,
-      ],
+    plugins: [
+        react(),
+        crx({manifest}),
+        zip({outDir: 'release', outFileName: `${name}-${version}.zip`}),
+    ],
+    server: {
+        cors: {
+            origin: [
+                /chrome-extension:\/\//,
+            ],
+        },
     },
-  },
 })
